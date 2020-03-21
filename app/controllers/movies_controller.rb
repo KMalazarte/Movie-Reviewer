195ef66a3282e1f09833f1aec35158393021a1f6
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-03-21 11:13:58 -0400

    added movie controller actions for admin to manipulate movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,11 +15,23 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.create(movie_params)
+    render json: @movie
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
   end
 
   def delete
+    @movie = Movie.find(params[:id])
+    @review.destroy
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :avg_score, :movie_img, :user_comment, :user_score, :created_at, :updated_at)
   end
 end
