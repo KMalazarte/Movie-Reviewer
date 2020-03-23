6d5d88e26c527ccab8c475db1eaf969459213216
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-03-23 00:27:09 -0400

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,11 @@ require 'byebug'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 def get_popular_movies()
   tmdb_key = ENV["API_KEY"]
   api = RestClient.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=#{tmdb_key}")
   movie_obj = JSON.parse(api)
 
-  # byebug
     data = movie_obj["results"]
 
     data.map{|movie| Movie.create(title: movie["title"], description: movie["overview"], avg_score: movie["vote_average"], movie_img: movie["poster_path"], user_comment: [])}
