5952be050c94707902bddc96b201c5683af6db70
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-03-23 11:39:47 -0400

    fix typo

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module BackEndNew
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, ,:put, :patch, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
